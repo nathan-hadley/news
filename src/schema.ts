10d4698d90b28f8192c675aa07dedb4abb1acfd0
Nathan Hadley <nathan.hadley@me.com>
2023-04-01 17:14:15 -0400

    add mutation

--- src/schema.ts
+++ src/schema.ts
@@ -19,12 +19,22 @@ const resolvers = {
     info: () => `This is the API of a news app.`,
     feed: () => links,
   },
-  Link: {
-    id: (parent: Link) => parent.id,
-    description: (parent: Link) => parent.description,
-    url: (parent: Link) => parent.url,
-  }
-}
+  Mutation: {
+    post: (parent: unknown, args: { description: string, url: string }) => {
+      let idCount = links.length;
+
+      const link: Link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+
+      links.push(link);
+
+      return link;
+    },
+  },
+};
 
 export const schema = makeExecutableSchema({
   typeDefs,
